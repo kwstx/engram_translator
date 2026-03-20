86b6647a114f8b5b21ef64f14c9e71967a6156fe
kwstx <kwstasgalan32@gmail.com>
2026-03-20 22:40:52 +0200

    creating unified trade schema

---
+++ schemas/unified-trade-schema.js
@@ -0,0 +1,22 @@
+module.exports = {
+  tradeOrder: {
+    action: 'buy | sell | market | limit',
+    symbol: 'string',
+    quantity: 'number',
+    price: 'optional number',
+    side: 'buy | sell'
+  },
+  balanceQuery: {
+    asset: 'string'
+  },
+  paymentIntent: {
+    amount: 'number',
+    currency: 'usd',
+    customerId: 'optional string'
+  },
+  feedRequest: {
+    source: 'x | fred | reuters',
+    query: 'string',
+    limit: 'number'
+  }
+};
